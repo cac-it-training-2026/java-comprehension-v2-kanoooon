e79db043763863614c977ab76335a4abd91264e1
kanoni <kanoni@C010422963.zipangu.cac.co.jp>
2026-04-28 16:08:45 +0900

    0428  q01_basic.question03まで完了
    理解できた点
    ・コンストラクタの引数ありとなしの違い
    ・クラスから呼び出し方
    
    不明な点・調査中
    ・初歩的な処理の書き方
    ・コンストラクタやインスタンス、拡張for文などの使い方、書き方。違い

--- java_comprehension_exercises_volume2_NO_NAME/src/q01_basic/question04/MemberManager.java
+++ java_comprehension_exercises_volume2_NO_NAME/src/q01_basic/question04/MemberManager.java
@@ -1,11 +1,19 @@
 package q01_basic.question04;
 
 class MemberManager {
+	public static void showAllMember(Member[] information) {
+		for (Member i : information) {
+			i.showMember();
+
+		}
+
+	}
 
 	/**
 	 * インスタンス化の禁止
 	 */
 	private MemberManager() {
+
 	}
 
 	//TODO showAllMembersメソッドを実装する
